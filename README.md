6b7335dcd6a6099a3e0058ffed93357612c0bae7
Shikhir Arora <shikhir@sarora.io>
2024-08-05 07:57:27 -0400

    Rework 2024

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/08cb3f80-c508-4215-9bd1-e04f0b92611f/deploy-status)](https://app.netlify.com/sites/thesarogroup/deploys)
--- asynctk.js
+++ asynctk.js
@@ -1,1 +1,39 @@
-var _0x4923=['documentElement','className','replace','\x20wf-inactive','script','getElementsByTagName','src','https://use.typekit.net/','kitId','async','onload','onreadystatechange','complete','loaded','load','parentNode','insertBefore','fcg5qzu'];(function(_0x565589,_0x3e57d2){var _0x587b12=function(_0x660158){while(--_0x660158){_0x565589['push'](_0x565589['shift']());}};_0x587b12(++_0x3e57d2);}(_0x4923,0x1af));var _0x2b33=function(_0x194715,_0x4b3286){_0x194715=_0x194715-0x0;var _0x309325=_0x4923[_0x194715];return _0x309325;};(function(_0xd362df){var _0x4b9aeb={'kitId':_0x2b33('0x0'),'scriptTimeout':0xbb8,'async':!![]},_0x157690=_0xd362df[_0x2b33('0x1')],_0x3c4dc6=setTimeout(function(){_0x157690[_0x2b33('0x2')]=_0x157690['className'][_0x2b33('0x3')](/\bwf-loading\b/g,'')+_0x2b33('0x4');},_0x4b9aeb['scriptTimeout']),_0x5dc48c=_0xd362df['createElement'](_0x2b33('0x5')),_0x1f0d51=![],_0x31ca52=_0xd362df[_0x2b33('0x6')](_0x2b33('0x5'))[0x0],_0x22e48d;_0x157690['className']+='\x20wf-loading';_0x5dc48c[_0x2b33('0x7')]=_0x2b33('0x8')+_0x4b9aeb[_0x2b33('0x9')]+'.js';_0x5dc48c[_0x2b33('0xa')]=!![];_0x5dc48c[_0x2b33('0xb')]=_0x5dc48c[_0x2b33('0xc')]=function(){_0x22e48d=this['readyState'];if(_0x1f0d51||_0x22e48d&&_0x22e48d!=_0x2b33('0xd')&&_0x22e48d!=_0x2b33('0xe'))return;_0x1f0d51=!![];clearTimeout(_0x3c4dc6);try{Typekit[_0x2b33('0xf')](_0x4b9aeb);}catch(_0x12953e){}};_0x31ca52[_0x2b33('0x10')][_0x2b33('0x11')](_0x5dc48c,_0x31ca52);}(document));
+((document) => {
+  const config = {
+    kitId: "fcg5qzu",
+    scriptTimeout: 3000,
+    async: true,
+  };
+
+  const htmlElement = document.documentElement;
+  const scriptElement = document.createElement("script");
+  let timeout;
+
+  const clearLoadingClass = () => {
+    htmlElement.className =
+      htmlElement.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+  };
+  timeout = setTimeout(clearLoadingClass, config.scriptTimeout);
+
+  htmlElement.className += " wf-loading";
+
+  scriptElement.src = `https://use.typekit.net/${config.kitId}.js`;
+  scriptElement.async = true;
+
+  scriptElement.onload = scriptElement.onreadystatechange = function () {
+    const readyState = this.readyState;
+    if (readyState && readyState !== "complete" && readyState !== "loaded")
+      return;
+
+    clearTimeout(timeout);
+
+    try {
+      Typekit.load(config);
+    } catch (e) {
+      console.error("Error loading Typekit:", e);
+    }
+  };
+
+  const firstScript = document.getElementsByTagName("script")[0];
+  firstScript.parentNode.insertBefore(scriptElement, firstScript);
+})(document);
